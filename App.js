c2ace4c425ecaa1289673f9999ae1e08bfd6d1e5
Keying <Keying01152000@gmail.com>
2024-12-04 01:15:22 -0800

    addAll

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import session from "express-session";
 import express from "express";
+import mongoose from "mongoose";
 import cors from "cors";
 import Hello from "./Hello.js"
 import Lab5 from "./Lab5/index.js";
@@ -9,6 +10,10 @@ import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import AssignmentRoutes from "./Kanbas/Courses/Assignments/routes.js";
 import EnrollmentRoutes from "./Kanbas/Enrollments/routes.js";
+
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
+mongoose.connect(CONNECTION_STRING);
+
 const app = express()
 
 app.use(cors({
